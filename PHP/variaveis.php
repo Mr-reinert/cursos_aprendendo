347c439e0adf075ee1bc2e91a868521190b168c8
Mr-reinert <reinert.neto@gmail.com>
2022-10-03 22:51:38 -0300

    Create variaveis.php

---
+++ PHP/variaveis.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		$string = "não é necessário a identificação do tipo da variável, pois ao analizar seu conteúdo, o interpretador já faz essa análise";
+		$n1 = 4;
+		$n2 = 4.76594;
+		$n3 = -1;
+		$logic = false; 
+
+		echo "$string. da mesma forma acontece com os números por ex: $n1, $n2, n3";
+	?>
+</body>
+</html>
