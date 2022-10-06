514ffcc467bed8a60bfb25e8315081ad2120669c
Mr-reinert <reinert.neto@gmail.com>
2022-10-05 23:15:47 -0300

    formulario_nome_idade

---
+++ PHP/html+php/02idadee.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+	<div>
+	<?php 
+		$nome = isset($_GET["nome"])?$_GET["nome"] : "[não informado]";
+		$ano = isset($_GET["ano"])?$_GET["ano"] :0;
+		$sexo = isset($_GET["sexo"])?$_GET["sexo"]:"[não informado]";
+
+		$idade= date("Y") - $ano;
+
+		echo "$nome é $sexo e tem $idade anos";
+
+	?>
+	<br>
+	<a href="formulario.html">Voltar</a>
+	</div>
+</body>
+</html>
